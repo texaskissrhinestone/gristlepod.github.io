0ab169a1ad361f607e9e5edb4ba4c3e248de840c
0ekekddddd <0ekekddddd@protonmail.com>
2025-02-06 04:38:06 -0600

    .

--- rss/feedDisplay.js
+++
@@ -1,7 +0,0 @@
-var container = document.createElement("div");
-container.innerHTML = "<p>Sorry, your request could not be processed:<\/p>";
-var nodes = [].slice.call(container.childNodes);
-var script = document.scripts[document.scripts.length - 1];
-var parent = script.parentNode;
-while(nodes.length > 0) parent.insertBefore(nodes.shift(), script);
-parent.removeChild(script);
