6e079645248a355d67ad12c5194348b33379ff19
0ekekddddd <0ekekddddd@protonmail.com>
2025-05-10 15:04:11 -0500

    .

---
+++ jfashion/content.js
@@ -0,0 +1,7 @@
+function lContainer(){
+  document.write("<h4>Entrance</h4><ul><li><a href='.'>Home</a></li><li><a href='../'>Back to Homepage</a></li></ul><h4>The Mirror</h4><ul><li><a href='about-gyaruo-aristo.html'>What is Gyaruo Fashion &amp; Aristo?</a></li><li><a href='100questions.html'>100 Questions For An Ouji (2025)</a></li><li><a href='links.html'>Resources &amp; Links</a></li></ul><h4>The Wardrobe</h4><ul><li><strike><a href='closet.html'>Closet Archive</a></strike></li><li><a href='itabag.html'>痛バ ItaBag Diary</a></li><li><strike><a href='coordinategallery.html'>Coordinate Gallery</a></strike></li><li><strike><a href='wishlist.html'>Wishlist</a></strike></li><li><strike><a href='scans.html'>Scans</a></strike></li></ul>");
+}
+
+function footer(){
+  document.write("<img src='layout_pieces/sunflowers_divider.gif' alt=''><br><a href='https://metamorphose.gr.jp/en/online-shop'><img src='layout_pieces/metamorphose_badge.png' alt='Metamorphose' title='Metamorphose'></a><br><img src='../ui/buttons/gyaruouser_badge.jpg' alt='This guy is a gyaruo!' title='This guy is a gyaruo!'>");
+}
