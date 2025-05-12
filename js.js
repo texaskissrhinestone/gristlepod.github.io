16f237ca463c48bf3990b59d74ea690850aed93c
0ekekddddd <0ekekddddd@protonmail.com>
2025-05-12 07:02:46 -0500

    .

---
+++ js.js
@@ -0,0 +1,11 @@
+function nav(){
+  document.write('<a href=".">home</a> | <a href="store.html">store</a> | <a href="terms.html">terms</a><div class="navBorder"></div>');
+}
+
+function socialLinks(){
+  document.write('<div id="links"><a href="https://ko-fi.com/gristlepod"><button style="background-image:url(&apos;ui/icons/kofi.png&apos;);" alt="Ko-fi store" title="Ko-fi store"></button></a><a href="https://bsky.app/profile/gristlepod.bsky.social"><button style="background-image:url(&apos;ui/icons/bsky.png&apos;);" alt="Bluesky" title="Bluesky"></button></a><a href="https://x.com/ProspectorID5"><button style="background-image:url(&apos;ui/icons/twitter.png&apos;);" alt="Twitter" title="Twitter"></button></a><a href="https://gristlepod.github.io/rss.xml"><button style="background-image:url(&apos;ui/icons/rss.png&apos;);" alt="Twitter" title="Twitter"></button></a></div>');
+}
+
+function footer(){
+  document.write("© 2025 Gristlepod. All rights reserved.");
+}
