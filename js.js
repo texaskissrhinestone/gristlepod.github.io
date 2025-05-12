9df4aa6c0c7bd22351af93886e463995d51fdff8
0ekekddddd <0ekekddddd@protonmail.com>
2025-05-12 12:08:09 -0500

    .

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ function nav(){
 }
 
 function socialLinks(){
-  document.write('<div id="links"><a href="https://ko-fi.com/gristlepod"><button style="background-image:url(&apos;ui/icons/kofi.png&apos;);" alt="Ko-fi store" title="Ko-fi store"></button></a><a href="https://bsky.app/profile/gristlepod.bsky.social"><button style="background-image:url(&apos;ui/icons/bsky.png&apos;);" alt="Bluesky" title="Bluesky"></button></a><a href="https://x.com/ProspectorID5"><button style="background-image:url(&apos;ui/icons/twitter.png&apos;);" alt="Twitter" title="Twitter"></button></a><a href="https://gristlepod.github.io/rss.xml"><button style="background-image:url(&apos;ui/icons/rss.png&apos;);" alt="Twitter" title="Twitter"></button></a></div>');
+  document.write('<div id="links"><a href="https://ko-fi.com/gristlepod"><button style="background-image:url(&apos;ui/icons/kofi.png&apos;);" alt="Ko-fi store" title="Ko-fi store"></button></a><a href="https://bsky.app/profile/gristlepod.bsky.social"><button style="background-image:url(&apos;ui/icons/bsky.png&apos;);" alt="Bluesky" title="Bluesky"></button></a><a href="https://x.com/ProspectorID5"><button style="background-image:url(&apos;ui/icons/twitter.png&apos;);" alt="Twitter" title="Twitter"></button></a><a href="https://gristlepod.github.io/rss.xml"><button style="background-image:url(&apos;ui/icons/rss.png&apos;);" alt="RSS feed" title="Subscribe to the RSS."></button></a></div>');
 }
 
 function footer(){
